40aacc6b7d2568aedc512270db72282d2bfd3452
postaldude <sema091103@mail.ru>
2023-11-20 21:54:15 +0700

    COMMIT

--- src/api/index.ts
+++ src/api/index.ts
@@ -11,8 +11,8 @@ const getJson = async <T>(endpoint: ENDPOINTS): Promise<T> => {
   const path =
     process.env.NODE_ENV === 'development'
       ? `http://localhost:3001/api/${endpoint}`
-      : `https://raw.githubusercontent.com/robzarel/gh-pages-demo/gh-pages/static/db/${endpoint}.json`;
-
+      :`https://raw.githubusercontent.com/postaldude/gh-pages-demo/VIS3/static/db/${endpoint}.json`;
+      
   const response = await fetch(path);
 
   return await response.json();
